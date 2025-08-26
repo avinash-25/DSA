38574971a5f081d47b75e6fef0ce0e4e2d07de11
avinash-25 <avinashranjan268@gmail.com>
2025-08-26 20:37:33 +0530

    commit on 26 Aug 2025

--- Code/flow control statement/whileLoop.js
+++ Code/flow control statement/whileLoop.js
@@ -32,7 +32,8 @@ console.log("Total such numbers are : ", count);
 //! Print all the factors of a number(number itself included)
 
 
-/*
+/* Logic 1
+
 let num = 35;
 let temp = num - 1;
 while (temp >= 1) {
@@ -40,7 +41,105 @@ while (temp >= 1) {
         console.log(temp);
     temp--;
 }
+console.log(num);
 */
+
+//Logic 2
+
+/* 
+let num = 28;
+let i = 1;
+
+while (i <= num / 2) {
+    if (num % i == 0)
+        console.log(i);
+    i++;
+}
+console.log(num);
+*/
+
+
+// Logic 3
+
+/*
+if 203 is divisible by 7.
+Last digit = 3
+Double it = 6
+Remaining number = 20
+Subtract→ 20− 6 = 14
+
+let num = 203;
+
+let lastDigit = num % 10;
+let double = lastDigit * 2;
+let rem = num / 10;
+let n = rem - double;
+if (n % 7 == 0)
+    console.log("divisible");
+else
+    console.log("Not divisible");
+*/
+
+// Logic 4
+//! Best Logic
+
+/* 
+
+let num = 28;
+let i = 1;
+// let i = parseInt(Math.sqrt(45));
+//!       OR
+// let i = Math.floor(Math.sqrt(num));
+
+while (i * i <= num) {
+    if (num % i == 0) {
+        if (i == num / i)
+            console.log(i);
+        else {
+            console.log(num / i);
+            console.log(i);
+        }
+    }
+    i++;
+}
+
+*/
+
+//! Print all factor excuding self number.
+
+// let num = 28;
+// let i = 2;
+// console.log("1");
+// while (i * i <= num) {
+//     if (num % i == 0) {
+//         if (i == num / i)
+//             console.log(i);
+//         else {
+//             console.log(num / i);
+//             console.log(i);
+//         }
+//     }
+//     i++;
+// }
+
+//! Only count the factor of a number
+
+/*
+let num = 25;
+let count = 1;
+let i = 2;
+while (i * i <= num) {
+    if (num % i == 0) {
+        if (i == num / i)
+            count++;
+        else
+            count += 2;
+    }
+    i++;
+}
+console.log("NUmber of factors are : ", count);
+*/
+
 //! Question 09
 //! Count the total factors of the given number.
 
@@ -102,15 +201,15 @@ else
 //! Question 13
 //! Print the nth term of the fibobacci
 
-let num = 7;
-let a = 0
-let b = 1;
-let i = 2;
+// let num = 7;
+// let a = 0
+// let b = 1;
+// let i = 2;
 
-while (i <= num) {
-    c = a + b;
-    a = b;
-    b = c;
-    i++;
-}
-console.log("Last term is : ", c);
+// while (i <= num) {
+//     c = a + b;
+//     a = b;
+//     b = c;
+//     i++;
+// }
+// console.log("Last term is : ", c);
